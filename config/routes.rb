96b0078f2a4cbe17f6f664bfe2c1cd735c02a3d9
justinjkim <jjkim928@gmailcom>
2020-04-16 20:39:35 -0700

    add the reviews template

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :reviews
   root "movies#index"
 
-  resources :movies
-  # get "movies" => "movies#index"
-  # get "movies/:id" => "movies#show", as: "movie"
-  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  # patch "movies/:id" => "movies#update"
+  resources :movies do
+    resources :reviews
+  end
 end
