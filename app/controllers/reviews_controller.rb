1cb911120ea73da08010747456d6b7c5792d609b
justinjkim <jjkim928@gmailcom>
2020-04-25 19:28:02 -0700

    create online form for submitting reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,37 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
+  end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.new
+
+    @review.update(review_params)
+    if @review.save
+      redirect_to movie_reviews_url(@movie), notice: "Thank you for your review!"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def review_params
+    params.require(:review).permit(
+      :name,
+      :stars,
+      :comment
+    )
   end
 end
