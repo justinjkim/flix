07170a6c095e30548de52553c988bb6000bf7b1e
justinjkim <jjkim928@gmailcom>
2020-07-12 21:29:56 -0700

    update, create, destroy actions for User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,42 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user), notice: "Account was created!"
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "Account was successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, alert: "Account was deleted!"
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(:name, :email, :password, :password_confirmation)
   end
 end
