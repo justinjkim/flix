4870c466f8f0edba3acd544eaa48a991408ed7e6
justinjkim <jjkim928@gmailcom>
2020-07-14 14:05:19 -0700

    show indication a user is signed in by displaying their name in header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "Account was created!"
     else
       render :new
