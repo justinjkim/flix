418d54381ff0bf05c6c7f52fc620e43ceae76089
justinjkim <jjkim928@gmailcom>
2020-07-14 23:07:06 -0700

    remove duplication, implement require_correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path, notice: "Account was successfully updated!"
     else
@@ -38,13 +37,19 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil # need this line, otherwise app raises an exception when you delete an account, because we deleted the :user_id key, reference current_user method
     redirect_to movies_url, alert: "Account was deleted!"
   end
 
   private
+
+  def require_correct_user
+    @user = User.find(params[:id]) # this @user gets called for edit, update, destroy actions
+    unless current_user == @user
+      redirect_to movies_url # redirect if user is not signed in
+    end
+  end
 
   def user_params
     params.require(:user)
