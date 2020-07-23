e21da169c7d44ea6580fd7ee3d1da5c383cb5304
justinjkim <jjkim928@gmailcom>
2020-07-23 14:28:42 -0700

    establish relationship between User and Reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  belongs_to :user
 
-  validates :name, presence: true
   validates :comment, length: { minimum: 4 }
 
   STARS = [1,2,3,4,5]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :reviews, dependent: :destroy # establish one-many relationship with reviews
+
   has_secure_password
 
   validates :name, presence: true
