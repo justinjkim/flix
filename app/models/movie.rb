80880b9e64954f63841361dbb07e8a5e76179bf9
justinjkim <jjkim928@gmailcom>
2020-07-24 15:39:25 -0700

    establish relationship bw movie and genre through characterizations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user # many to many relationship
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
 
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
