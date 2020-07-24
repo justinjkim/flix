22d3a46ef5ace46bf3dadf680bfa2ab7cf0346ab
justinjkim <jjkim928@gmailcom>
2020-07-24 15:36:02 -0700

    create Characterization model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_221811) do
+ActiveRecord::Schema.define(version: 2020_07_24_223446) do
+
+  create_table "characterizations", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_characterizations_on_genre_id"
+    t.index ["movie_id"], name: "index_characterizations_on_movie_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "movie_id", null: false
@@ -60,6 +69,8 @@ ActiveRecord::Schema.define(version: 2020_07_24_221811) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "characterizations", "genres"
+  add_foreign_key "characterizations", "movies"
   add_foreign_key "favorites", "movies"
   add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "movies"
