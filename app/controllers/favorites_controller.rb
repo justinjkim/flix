b590f6ced862e2bb4f0ce4ed18c1935f436c7ad1
justinjkim <jjkim928@gmailcom>
2020-07-24 12:30:02 -0700

    allow signed in user to favorite a movie

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,12 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.favorites.create!(user: current_user)
+    # an alternative to the above line is:
+    # @movie.fans << current_user
+
+    redirect_to @movie # you need the redirect to refresh the page with the new count
+  end
 end
