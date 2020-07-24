9a161a8ea915cfcf7abc6aee77e38c8596026266
justinjkim <jjkim928@gmailcom>
2020-07-24 12:11:02 -0700

    nest favorites resource under movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :favorites
   root "movies#index"
 
   resource :session, only: [:new, :create, :destroy]
   get "signin" => "sessions#new"
-  
+
   resources :movies do
     resources :reviews
+    resources :favorites, only: [:create, :destroy] # nest favorites under movies so that specific movie is always specified
   end
 
   resources :users
