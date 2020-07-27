a3af20858a9218686de44301104745b846a709c1
justinjkim <jjkim928@gmailcom>
2020-07-27 09:42:48 -0700

    add slug column to Movies model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_223446) do
+ActiveRecord::Schema.define(version: 2020_07_27_164228) do
 
   create_table "characterizations", force: :cascade do |t|
     t.integer "movie_id", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_07_24_223446) do
     t.string "director"
     t.string "duration"
     t.string "image_file_name", default: "placeholder.png"
+    t.string "slug"
   end
 
   create_table "reviews", force: :cascade do |t|
