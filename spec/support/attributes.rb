58bb45a806f2efe49ee2362833c3dabe1299db32
justinjkim <jjkim928@gmailcom>
2020-08-16 11:57:39 -0700

    create movie attributes to avoid duplicating code

---
+++ spec/support/attributes.rb
@@ -0,0 +1,11 @@
+def movie_attributes(overrides = {})
+  {
+    title: "Iron Man",
+    rating: "PG-13",
+    total_gross: 318412101.00,
+    description: "Tony Stark builds an armored suit to fight the throes of evil",
+    director: "Anthony Russo",
+    duration: "120 min",
+    released_on: "2008-05-02"
+  }.merge(overrides)
+end
